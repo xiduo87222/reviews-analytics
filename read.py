94f49bea6c85cd181d02b266bc7c645835378997
Xiduo <iris5202@gmail.com>
2020-09-08 06:28:04 +0800

    add filtering

--- read.py
+++ read.py
@@ -6,9 +6,18 @@ with open ('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0: #如果count和1000的餘數是0
             print(len(data))
-print('檔案讀取完了,總共有',len(data),'筆資料')
+print('檔案讀取完了,總共有', len(data), '筆資料')
 
+#計算留言平均長度
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
-print('留言的平均長度為',sum_len/len(data))
+print('留言的平均長度為', sum_len/len(data))
+
+#篩選資料
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
